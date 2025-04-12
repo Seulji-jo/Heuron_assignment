2ec1eb8a7b40e41fa34c908b5a480e044b8b8953
Seulji-jo <joseuling@gmail.com>
2025-04-12 21:56:20 +0900

    Modify eslint - import 순서 정렬을 위해 패키지 설치 및 설정

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -23,4 +23,51 @@ export default defineConfig([
       'prettier/prettier': ['error'], // Prettier 규칙 위반 시 에러로 표시
     },
   },
+  {
+    plugins: ['import'],
+    extends: ['plugin:import/recommended'],
+    'import/order': [
+      'error',
+      {
+        groups: [
+          'builtin',
+          'external',
+          'internal',
+          'parent',
+          'sibling',
+          'object',
+          'type',
+          'index',
+          'unknown',
+        ],
+        pathGroups: [
+          {
+            pattern: 'react*',
+            group: 'external',
+            position: 'before',
+          },
+          {
+            pattern: 'react-dom',
+            group: 'external',
+            position: 'before',
+          },
+        ],
+
+        pathGroupsExcludedImportTypes: ['react'],
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+      },
+    ],
+    'sort-imports': [
+      'error',
+      {
+        ignoreCase: true,
+        ignoreDeclarationSort: true,
+        ignoreMemberSort: false,
+        allowSeparatedGroups: true,
+      },
+    ],
+  },
 ]);
