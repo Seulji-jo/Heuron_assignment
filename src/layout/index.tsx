5c6b68c646beb1fa9374bd1607945624d4220c5b
Seulji-jo <joseuling@gmail.com>
2025-04-14 00:44:14 +0900

    Setup Bootstrap - css 많은 시간을 쓰고 싶지 않았기 때문에 미리 디자인된 컴포넌트를 사용해 빠르게 개발하기 위해 bootstrap을 사용했습니다.

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -4,14 +4,26 @@ export default function Layout() {
   return (
     <div>
       <header>
-        <nav>
-          <NavLink to="/">Home</NavLink>
-          <NavLink to="/assign1">과제1</NavLink>
-          <NavLink to="/assign2">과제2</NavLink>
-          <NavLink to="/assign3">과제3</NavLink>
+        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+          <div className="container">
+            <ul className="navbar-nav my-3 gap-3">
+              <li className="nav-item">
+                <NavLink to="/">Home</NavLink>
+              </li>
+              <li>
+                <NavLink to="/assign1">과제1</NavLink>
+              </li>
+              <li>
+                <NavLink to="/assign2">과제2</NavLink>
+              </li>
+              <li>
+                <NavLink to="/assign3">과제3</NavLink>
+              </li>
+            </ul>
+          </div>
         </nav>
       </header>
-      <main>
+      <main className="container pt-4">
         <Outlet />
       </main>
     </div>
