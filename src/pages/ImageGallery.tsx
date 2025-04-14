68915cf07ea4cb943a7a177286ed83693fe141de
Seulji-jo <joseuling@gmail.com>
2025-04-14 22:45:41 +0900

    Setup Context for img color state

--- src/pages/ImageGallery.tsx
+++ src/pages/ImageGallery.tsx
@@ -1,8 +1,11 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { fetchImgList } from '../services';
 import { useQuery } from '@tanstack/react-query';
+import { ImgColorContext } from '../contexts/ImgColorContext';
 
 export default function ImageGallery() {
+  const [isColorImg, setIsColorImg] = useState(true);
+  const imgColor = isColorImg ? 'color' : 'gray';
   const { isLoading, data } = useQuery({
     queryKey: ['images'],
     queryFn: fetchImgList,
@@ -13,21 +16,36 @@ export default function ImageGallery() {
   }, [data]);
 
   return (
-    <div>
-      과제1
-      <div className="table-responsive">
-        <table className="table align-middle">
-          <thead></thead>
-          <tbody>
-            <tr>
-              <td>...</td>
-              <td>...</td>
-              <td>...</td>
-              <td>...</td>
-            </tr>
-          </tbody>
-        </table>
+    <ImgColorContext value={imgColor}>
+      <div>
+        과제1
+        <div className="form-check form-switch">
+          <input
+            className="form-check-input"
+            type="checkbox"
+            role="switch"
+            id="switch"
+            checked={isColorImg}
+            onChange={() => setIsColorImg(!isColorImg)}
+          />
+          <label className="form-check-label" htmlFor="switch">
+            {imgColor}
+          </label>
+        </div>
+        <div className="table-responsive">
+          <table className="table align-middle">
+            <thead></thead>
+            <tbody>
+              <tr>
+                <td>...</td>
+                <td>...</td>
+                <td>...</td>
+                <td>...</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </div>
-    </div>
+    </ImgColorContext>
   );
 }
