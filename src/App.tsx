ea8993575102de6c273c0e7d28af85b6bf4e533c
Seulji-jo <joseuling@gmail.com>
2025-04-18 00:47:22 +0900

    modify: contextApi App컴포넌트로 이동

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
+import { useState } from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ImgColorContext } from './contexts/ImgColorContext';
 import Router from './routes';
 
 const queryClient = new QueryClient();
 
 function App() {
+  const [isColorImg, setIsColorImg] = useState(true);
+
   return (
     <QueryClientProvider client={queryClient}>
-      <Router />
+      <ImgColorContext value={{ isColorImg, setIsColorImg }}>
+        <Router />
+      </ImgColorContext>
     </QueryClientProvider>
   );
 }
